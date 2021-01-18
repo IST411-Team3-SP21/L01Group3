086e6dfa63089e525112171f1502d0cdb1c6d930
Mark Tripoli <mark_tripoli@comcast.com>
2021-01-18 12:35:34 -0500

    Initial Setup

---
+++ src/main/java/edu/psu/l01group3/EchoClient.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.psu.l01group3;
+
+/**
+ *
+ * @author mtrip225
+ */
+public class EchoClient {
+    public static void main(String[] args) {
+    }
+}
