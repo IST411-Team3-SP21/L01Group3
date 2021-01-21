9e1ab90ff3dc58fefc17184e7de4bb9376ca79cc
Josiel Delgadillo <josieldelgadillo98@gmail.com>
2021-01-20 19:26:46 -0500

    Basic concept of group work

--- src/main/java/edu/psu/l01group3/EchoClient.java
+++ src/main/java/edu/psu/l01group3/EchoClient.java
@@ -4,12 +4,73 @@
  * and open the template in the editor.
  */
 package edu.psu.l01group3;
+/* 
+Project: Lab 1
+Purpose Details: Client-Server capitalization application
+Course: IST 411
+Author: Josiel Delgadillo
+Date Developed: 1/20/2021
+Last Date Changed:
+Revision: 1
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  *
- * @author mtrip225
+ * @author josieldelgadillo
  */
 public class EchoClient {
-    public static void main(String[] args) {
-    }
+    public static void main(String args[]) throws UnknownHostException {
+           System.out.println("Capitalizing Echo Client");
+           
+           try{
+               System.out.println("Client waiting on connection to server...");
+               InetAddress localAddress = InetAddress.getLocalHost();
+               
+               try(Socket clientSocket = new Socket(localAddress, 6000);
+                       PrintWriter out = new PrintWriter(
+                            clientSocket.getOutputStream(), true);
+                       BufferedReader br = new BufferedReader(
+                            new InputStreamReader(clientSocket.getInputStream()))){
+                   
+                   System.out.println("Client: Connected to server");
+                   
+                   // Get and handle input from Client
+                   Scanner scanner = new Scanner(System.in);
+                   while(true){
+                       System.out.println("Enter text: ");
+                       String inputLine = scanner.nextLine();
+                       
+                       // Change input from client to uppercase
+                       //inputLine = inputLine.toUpperCase();
+                       
+                       if("quit".equalsIgnoreCase(inputLine))
+                           break;
+                       
+                       out.println(inputLine);
+                       String response = br.readLine();
+                       System.out.println("Client: " + response);
+                   }
+                   
+               }
+               catch(IOException ex){
+                   //Handle Exception
+               }
+           }
+           catch(Exception ex){
+               // Handle Exception
+           }
+           
+           
+       }
+    
 }
+
--- src/main/java/edu/psu/l01group3/EchoServer.java
+++ src/main/java/edu/psu/l01group3/EchoServer.java
@@ -5,11 +5,47 @@
  */
 package edu.psu.l01group3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
- * @author mtrip225
+ * @author josieldelgadillo
  */
 public class EchoServer {
     public static void main(String[] args) {
+        System.out.println("Capitalizing Echo Server");
+        
+        //Create instance of ServerSocket
+        try(ServerSocket serverSocket = new ServerSocket(6000)){
+            
+            // Wait for connection to client
+            System.out.println("Server waiting for connection to client...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Server: Connected to client");
+            
+            try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                    clientSocket.getInputStream()));
+                    
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)){
+                
+                // inside try
+                // Repeat Client response
+                String inputLine;
+                while((inputLine = br.readLine()) != null){
+                    System.out.println("Server: " + inputLine);
+                    out.println(inputLine);
+                }
+            }
+            
+        }
+        catch(IOException ex){
+            // Handle exceptions
+        }
     }
 }
